02165645d86d4955aa5e740ba8e6d884f47d9179
Eagle19243 <eagle19243@gmail.com>
2020-08-07 08:27:09 +0300

    initial completion

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,21 @@
 const API_KEY = 'AIzaSyCuOlyB9ftUJiC-Qd6f2_QxvToT99TS--4';
 const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-const SPREADSHEET_ID = 'Y1VGV-DES4eTNmhuchcuKN9zvPKfnoICSEkb4H_2uio-U';
-const SPREADSHEET_TAB_NAME = 'Orders 5/20';
+const Colors = {
+    'AMEX-Bonvoy'   : { red: 0.275, green: 0.741, blue: 0.776 },
+    'AMEX-Blue'     : { red: 0.29,  green: 0.525, blue: 0.91  },
+    'AMEX_ABP (5%)' : { red: 0.706, green: 0.655, blue: 0.839 },
+    'P-3783'        : { red: 0.624, green: 0.773, blue: 0.91  },
+    'BB-3588'       : { red: 0.825, green: 0.651, blue: 0.741 },
+    'AMEX-Personal' : { red: 0.749, green: 0.565, blue: 0.0   },
+    'AMZ Credit'    : { red: 0.945, green: 0.761, blue: 0.196 },
+    'CHASE'         : { red: 0.976, green: 0.796, blue: 0.612 },
+    'Capital One'   : { red: 0.867, green: 0.494, blue: 0.42  },
+    'HD Gift Card'  : { red: 1.0,   green: 0.6,   blue: 0.0   },
+    'Merrick'       : { red: 0.576, green: 0.769, blue: 0.49  },
+    'Citizens'      : { red: 0.463, green: 0.647, blue: 0.686 },
+    'WF-3994'       : { red: 1.0,   green: 0.949, blue: 0.8   },
+    'AMEX-CHRIS'    : { red: 0.0,   green: 1.0,   blue: 0.0   },
+    'profit'        : { red: 0.714, green: 0.843, blue: 0.659 },
+    'roiFontColor'  : { red: 0.22,  green: 0.463, blue: 0.114 },
+    'default'       : { red: 1.0,   green: 0.949, blue: 0.8   }
+}
--- public/js/options.js
+++ public/js/options.js
@@ -7,8 +7,138 @@ async function init() {
     $('.btn-start').click(btnStartClicked);
 }
 
-function btnStartClicked() {
+async function btnStartClicked() {
+    showProgress('.btn-start');
+
+    const url           = $('#sheet_url').val();
+    const orderTab      = $('#orders_tab').val();
+    const paidOrderTab  = $('#paid_orders_tab').val();
+    const spreadsheetId = getSpreadsheetIdFromURL(url);
+
+    let gPaidOrderRes   = await gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: paidOrderTab
+    });
+    let gOrderRes       = await gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: orderTab
+    });
+    const gPaidOrders   = gPaidOrderRes.result.values;
+    const gOrders       = gOrderRes.result.values;
+    const gSpreadSheet  = await gapi.client.sheets.spreadsheets.get({
+        spreadsheetId: spreadsheetId
+    });
+
+    const gOrderSheetId     = getSheetIdByName(orderTab, gSpreadSheet.result.sheets);
+    const gPaidOrderSheetId = getSheetIdByName(paidOrderTab, gSpreadSheet.result.sheets);
+
+    const requestOrder = {
+        spreadsheetId: spreadsheetId,
+        resource: {
+            requests: []
+        }
+    };
+    const requestPaidOrder = {
+        spreadsheetId: spreadsheetId,
+        resource: {
+            requests: [
+                {
+                    appendCells: {
+                        sheetId: gPaidOrderSheetId,
+                        rows: [],
+                        fields: '*'
+                    }
+                }
+            ]
+        }
+    };
+
+    for (const paidOrder of paidOrders) {
+        const gOrderRowNum   = getRowNum(paidOrder, gOrders);
+        const gOrderColCount = gOrders[9].length;
+        const rowData        = { values: [] };
+        
+        if (gOrderRowNum == -1) {
+            continue;
+        }
+
+        if (getRowNum(paidOrder, gPaidOrders) > -1) {
+            continue;
+        }
+
+        requestOrder.resource.requests.push({
+            repeatCell: {
+                range: {
+                    sheetId: gOrderSheetId,
+                    startRowIndex: gOrderRowNum,
+                    endRowIndex: gOrderRowNum + 1,
+                    startColumnIndex: 0,
+                    endColumnIndex: gOrders[9].length
+                },
+                fields: 'userEnteredFormat.backgroundColor',
+                cell: {
+                    userEnteredFormat: {
+                        backgroundColor: Colors.default
+                    }
+                }
+            }
+        });
+
+        gOrders[gOrderRowNum].forEach((value, col) => {
+            const cellData      = {
+                userEnteredValue: {
+                    stringValue: value
+                },
+                userEnteredFormat: {
+                    backgroundColor: Colors.default,
+                    horizontalAlignment: 3
+                }
+            }
+            const leftCols      = [0, 6];
+            const centerCols    = [2, 5]
+            let backgroundColor = Colors.default;
+            let alignment       = 3;
+
+            if (leftCols.includes(col)) {
+                alignment = 1;
+            } else if (centerCols.includes(col)) {
+                alignment = 2;
+            }
+
+            if (col == 0) {
+                backgroundColor = Colors[value] || Colors.default;
+            } else if (col == 11) {
+                backgroundColor = rowData.values[0].userEnteredFormat.backgroundColor;
+            } else if (col == 14) {
+                backgroundColor = Colors.profit;
+            } else if (col > 14 && col < 29) {
+                if (value != '') {
+                    backgroundColor = rowData.values[0].userEnteredFormat.backgroundColor;
+                } else {
+                    backgroundColor = Colors.default;
+                }
+            }
+
+            cellData.userEnteredFormat.backgroundColor = backgroundColor;
+            cellData.userEnteredFormat.horizontalAlignment = alignment;
+            
+            if (col == 3) {
+                cellData.userEnteredFormat.textFormat = {
+                    foregroundColor: Colors.roiFontColor,
+                    bold: true
+                };
+            }
+
+            rowData.values.push(cellData);
+        });
+
+        requestPaidOrder.resource.requests[0].appendCells.rows.push(rowData);
+    }
     
+    gOrderRes = await gapi.client.sheets.spreadsheets.batchUpdate(requestOrder);
+    gPaidOrderRes = await gapi.client.sheets.spreadsheets.batchUpdate(requestPaidOrder);   
+
+    hideProgress('.btn-start');
 }
 
 function fileChanged() {
@@ -42,7 +172,6 @@ function onGoogleAPILoad() {
         discoveryDocs: DISCOVERY_DOCS,
     }).then(function () {
         chrome.identity.getAuthToken({interactive: true}, function(token) {
-            console.log(token);
             gapi.auth.setToken({
               'access_token': token,
             });
@@ -50,6 +179,37 @@ function onGoogleAPILoad() {
     }, function(error) {
         console.log('error', error);
     });
+}
+
+/**
+ * Return index of array that contains a value
+ * @param {string} value 
+ * @param {array} array 
+ */
+function getRowNum(value, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].includes(value)) {
+            return i;
+        }
+    }
+
+    return -1
+}
+
+function getSheetIdByName(name, sheets)  {
+    for (const sheet of sheets) {
+        if (sheet.properties.title == name) {
+            return sheet.properties.sheetId;
+        }
+    }
+
+    return null;
+}
+
+function getSpreadsheetIdFromURL(url) {
+    const regExp = /https:\/\/docs\.google\.com\/spreadsheets\/d\/(.*)\/edit/g;
+    const match = regExp.exec(url);
+    return match ? match[1] : null;
 }
 
 /**
